e49016d3e0444793c1576b3551116738c913df79
HurtworldMap <rogernobrega@live.com>
2016-08-06 19:19:22 -0300

    new icons

--- js.js
+++ js.js
@@ -1,12 +1,32 @@
+https://atlasdatabase.github.io/img/plutonium.png
+
 var itemslang = {
     "en": {
         "translation": {
           "plutonium": "Plutonium",
+          "carbon": "Carbon",
+          "heridium": "Heridium",
+          "iron": "Iron",
+          
+          "isotope-commom": "Isotope Element - Commom",
+          "isotope-rare": "Isotope Element - Rare",
+          "silicate-common": "Silicate Element - Common",
+          "oxide-common": "Oxide Element - Common",
+          
         }
     },
         "br": {
         "translation": {
           "plutonium": "Plutônio",
+          "carbon": "Carbono",
+          "heridium": "Heridium",
+          "iron": "Ferro",
+          
+          "isotope-commom": "Elemento Isótopo - Comum",
+          "isotope-rare": "Elemento Isótopo - Raro",
+          "silicate-common": "Elemento Silicato - Comum",
+          "oxide-common": "Elemento Óxido - Comum",
+          
 		}
 	}
 };
